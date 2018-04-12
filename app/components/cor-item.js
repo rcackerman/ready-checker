5aca87bee67d95c416cee38239a2a8ca154b4eb3
Rebecca <rebecca@casecommons.org>
2018-04-12 11:20:01 -0400

    Adding toggling functionality

--- app/components/cor-item.js
+++ app/components/cor-item.js
@@ -1,4 +1,15 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  peopleReady: true,
+
+  actions: {
+    setNotReady() {
+      let promise = this.get('togglePNR')();
+      console.info(promise)
+      promise.then(() => {
+        this.set('peopleReady', false);
+      });
+    }
+  }
 });
--- app/components/cor-list.js
+++ app/components/cor-list.js
@@ -1,4 +1,12 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  anyPNR: false,
+
+  actions: {
+    toggleAnyPNR() {
+      console.info('toggled');
+      return true;
+    }
+  }
 });
